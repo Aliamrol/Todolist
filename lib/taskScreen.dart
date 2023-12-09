d6c4acc7687f6571f5c944cc27bd7d3d069dea5a
Aliamrol <Aliamrol@aut.ac.ir>
2023-12-09 04:47:33 +0330

    add textfield and icon to task screen

--- lib/taskScreen.dart
+++ lib/taskScreen.dart
@@ -10,24 +10,41 @@ class taskScreenState extends State<taskScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("Write your task details"),
-
-        ],
-      ),
-      appBar: AppBar(
-        title: Text(
-          "Subject",
-          style: TextStyle(fontWeight: FontWeight.bold),
+        body: SafeArea(
+      child: Container(
+        padding: EdgeInsets.only(top: 20, right: 20, left: 20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(
+                    Icons.arrow_back_rounded,
+                    color: Colors.grey[700],
+                    size: 30,
+                  ),
+                ),
+                Expanded(
+                    child: Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: TextField(
+                    decoration: InputDecoration(hintText: "Subject Task"),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                      fontSize: 24
+                    ),
+                  ),
+                ))
+              ],
+            )
+          ],
         ),
-        leading: IconButton(
-          icon: Icon(Icons.keyboard_return),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
       ),
-    );
+    ));
   }
 }
