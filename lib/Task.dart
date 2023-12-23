dbd3a891df62ca5dd9fa35c66aea67aea8d7f43e
Aliamrol <Aliamrol@aut.ac.ir>
2023-12-23 22:20:06 +0330

    delete lib folder

--- lib/Task.dart
+++
@@ -1,7 +0,0 @@
-class Task {
-  final int id;
-  late String title;
-  String? description;
-
-  Task({required this.title, required this.id, this.description});
-}
--- lib/Todo.dart
+++
@@ -1,6 +0,0 @@
-class Todo {
-  final title;
-  final isdone;
-
-  Todo({required this.title, this.isdone = false});
-}
