3f1df853ddbd38ed4059efc2591287d0169c9a12
ali amrol <aliamrol@aut.ac.ir>
2023-12-31 14:30:59 +0330

    fix Bugs : SetState after call functions

--- lib/Screens/DeletedTasksScreen.dart
+++ lib/Screens/DeletedTasksScreen.dart
@@ -22,18 +22,39 @@ class deletedTasksScreen extends StatefulWidget {
 class _deletedTasksScreen extends State<deletedTasksScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
+  deleteTaskSetState(Task t) {
+    setState(() {
+      widget.deletedTasks =
+          widget.deletedTasks.where((element) => element.id != t.id).toList();
+      widget.deleteTaskSure(t);
+    });
+  }
+
+  returnTaskSetState(Task t) {
+    setState(() {
+      widget.deletedTasks =
+          widget.deletedTasks.where((element) => element.id != t.id).toList();
+      widget.returnTask(t);
+    });
+  }
+
+  int i = 0;
+
   @override
   Widget build(BuildContext context) {
+    i += 1;
+    print("DeletedTask : ${widget.deletedTasks.length}");
     List<Widget> rows = [];
     for (int i = 0; i < widget.deletedTasks.length; i++) {
-      rows.add(SizedBox(
-        height: 20,
-      ));
+      // rows.add(SizedBox(
+      //   height: 20,
+      // ));
       rows.add(taskItemDeleted(
           task: widget.deletedTasks[i],
-          deleteTask: widget.deleteTaskSure,
-          returnTask: widget.returnTask));
+          deleteTask: deleteTaskSetState,
+          returnTask: returnTaskSetState));
     }
+    // print("Rows:  ${widget.deletedTasks.length}");
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
